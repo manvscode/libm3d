7d0521bc5164345da46637b843107146c8875f16
Joe Marrero <manvscode@gmail.com>
2014-06-17 13:10:34 -0400

    Added intrinsic headers.

--- src/mathematics.h
+++ src/mathematics.h
@@ -24,6 +24,20 @@
 #include <float.h>
 #include <stdbool.h>
 #include "lib3dmath-config.h"
+#if defined(_MSC_VEC)
+#include <intrin.h>  /* Microsoft C/C++-compatible compiler */
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#include <x86intrin.h> /* GCC-compatible compiler, targeting x86/x86-64 */
+#elif defined(__GNUC__) && defined(__ARM_NEON__)
+#include <arm_neon.h> /* GCC-compatible compiler, targeting ARM with NEON */
+#elif defined(__GNUC__) && defined(__IWMMXT__)
+#include <mmintrin.h> /* GCC-compatible compiler, targeting ARM with WMMX */
+#elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
+#include <altivec.h> /* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
+#elif defined(__GNUC__) && defined(__SPE__)
+#include <spe.h> /* GCC-compatible compiler, targeting PowerPC with SPE */
+#endif
+
 
 #ifdef __cplusplus
 extern "C" {
